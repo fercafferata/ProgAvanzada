ea11d154e472cbb356ed9c4afce8bc6f8772c71c
fercafferata <50627758+fercafferata@users.noreply.github.com>
2020-08-19 10:53:41 -0300

    Delete Desafio1Ej2.js

--- Desafio1/Desafio1Ej2.js
+++
@@ -1,8 +0,0 @@
-let num1 = 10;
-let num2 = 5;
-
-console.log(`Los numeros son: ${num1} y ${num2}`);
-console.log(`La suma de los números es ${num1 + num2}`);
-console.log(`La resta de los números es: ${num1 - num2}`);
-console.log(`La multiplicacion de los números es: ${num1 * num2}`);
-console.log(`La division de los números es: ${num1 / num2}`);
