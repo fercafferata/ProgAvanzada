7cafa9b1f821e939a7f529f558c15537ba3f75a2
fercafferata <50627758+fercafferata@users.noreply.github.com>
2020-08-31 14:52:37 -0300

    Add files via upload

--- Tp2/Ejercicio2.js
+++ Tp2/Ejercicio2.js
@@ -1,13 +1,18 @@
 const Datos = async() => {
+    
     const apikey = '800ee3a851285b5d430918fce0b3e647';
     const respuesta = await fetch(`http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${apikey}&format=json&limit=10`);
     const {artists} = await respuesta.json();
+    
     console.log(artists);
-    let crearTabla = `<tr>
+    
+    let crearTabla = 
+                    `<tr>
                         <td>ARTISTA</td>
                         <td>REPRODUCCIONES</td>
                         <td>OYENTES</td>
-                    </tr>`
+                    </tr>`;
+    
     artists.artist.forEach(artista => {
         crearTabla += `
         <tr>
@@ -16,6 +21,7 @@ const Datos = async() => {
             <td>${artista.listeners}</td>
         </tr>
         `;
+        
         document.querySelector("table").innerHTML = crearTabla;
     });
 
